5c71eaadab280af80727f12f31056c4c4bc6113d
madz <markdiesta@gmail.com>
2019-12-10 00:49:48 +0800

    Refactor showing of error.

--- lib/pages/forgot_password.dart
+++ lib/pages/forgot_password.dart
@@ -2,68 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:flutter_examples/auth/auth.dart';
 import 'package:regexed_validator/regexed_validator.dart';
 
-class ForgotPasswordPage extends StatefulWidget {
-  @override
-  _ForgotPasswordPageState createState() => _ForgotPasswordPageState();
-}
+final Auth _auth = Auth();
 
-class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
-  final Auth _auth = Auth();
+final _formKey = GlobalKey<FormState>();
 
-  final _formKey = GlobalKey<FormState>();
+final _emailController = TextEditingController();
 
-  TextEditingController _emailController = TextEditingController();
-
+class ForgotPasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Forgot Password"),
       ),
-      body: Center(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                child: TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Email',
+      body: Builder(
+        builder: (BuildContext context) {
+          return Center(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                    child: TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Email',
+                      ),
+                      validator: (String value) {
+                        return !validator.email(_emailController.text)
+                            ? 'Email is invalid.'
+                            : null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                    ),
                   ),
-                  validator: (String value) {
-                    return !validator.email(_emailController.text)
-                        ? 'Email is invalid.'
-                        : null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              RaisedButton(
-                child: Text(
-                  "Reset",
-                  style: TextStyle(
-                    color: Colors.white,
+                  SizedBox(height: 16.0),
+                  RaisedButton(
+                    child: Text(
+                      "Reset",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    color: Colors.blue,
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _auth.sendForgotPasswordLink(
+                            context, _emailController.text);
+                      } else {
+                        debugPrint("Invalid form");
+                      }
+                    },
                   ),
-                ),
-                color: Colors.blue,
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    _auth.sendForgotPasswordLink(
-                        context, _emailController.text);
-                  } else {
-                    debugPrint("Invalid form");
-                  }
-                },
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        }
       ),
     );
   }
